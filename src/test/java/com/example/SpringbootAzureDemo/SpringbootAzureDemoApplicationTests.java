22b8508b240245bb02c605c4313b3dcb0c7dca81
Arvind Awasthi <arvind.awasthi@hcl.com>
2023-01-25 17:00:27 +0530

    first commit

---
+++ src/test/java/com/example/SpringbootAzureDemo/SpringbootAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootAzureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
